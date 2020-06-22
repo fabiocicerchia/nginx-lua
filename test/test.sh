c0f4e40130290c67074f0c44e31ab06b2f85ea2f
fabiocicerchia <info@fabiocicerchia.it>
2020-06-22 23:03:25 +0200

    fixed test script

--- test/test.sh
+++ test/test.sh
@@ -5,12 +5,18 @@ source supported_versions
 function test() {
     DOCKER_TAG=$1
 
-    docker image ls -q fabiocicerchia/nginx:$DOCKER_TAG \
-        || (docker run -d --name nginx_lua_test -p 8080:80 -v $PWD/test/nginx.conf:/etc/nginx/nginx.conf fabiocicerchia/nginx-lua:$DOCKER_TAG \
-        && until $(curl --output /dev/null --silent --head --fail http://localhost:8080); do echo -n '.'; sleep 0.5; done \
-        ; curl -v http://localhost:8080 | grep "Welcome to nginx" || exit 1 \
-        ; curl -v http://localhost:8080/lua_content | grep "Hello world" || exit 1 \
-        ; docker rm -f nginx_lua_test)
+    docker rm -f nginx_lua_test 2> /dev/null
+
+    FOUND=$(docker image ls -q fabiocicerchia/nginx-lua:$DOCKER_TAG | wc -l)
+    if [ $FOUND -ne 0 ]; then
+        docker run -d --name nginx_lua_test -p 8080:80 -v $PWD/test/nginx.conf:/etc/nginx/nginx.conf fabiocicerchia/nginx-lua:$DOCKER_TAG \
+            && until $(curl --output /dev/null --silent --head --fail http://localhost:8080); do echo -n '.'; sleep 0.5; done \
+            ; curl -v http://localhost:8080 | grep "Welcome to nginx" || exit 1 \
+            ; curl -v http://localhost:8080/lua_content | grep "Hello world" || exit 1 \
+            ; docker rm -f nginx_lua_test
+     else
+        echo "Image not found: fabiocicerchia/nginx-lua:$DOCKER_TAG"
+     fi
 }
 
 function runtest() {
