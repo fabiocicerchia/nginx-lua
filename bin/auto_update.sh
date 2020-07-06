21f17f9b94aeff664c5484d4bc936c807d4cce07
fabiocicerchia <info@fabiocicerchia.it>
2020-07-06 19:56:05 +0200

    fail the auto update script if needed

--- bin/auto_update.sh
+++ bin/auto_update.sh
@@ -3,9 +3,9 @@
 
 set -x
 
-./bin/generate_supported_versions.sh
-./bin/dockerfile-generate.sh
-./bin/generate_tags.py | tee docs/TAGS.md
+./bin/generate_supported_versions.sh || exit 1
+./bin/dockerfile-generate.sh || exit 1
+./bin/generate_tags.py | tee docs/TAGS.md || exit 1
 git config --global user.name "fabiocicerchia"
 git config --global user.email "fabiocicerchia@users.noreply.github.com"
 git add -A
