caf131c508c02692bf0bb9ad59930ce50de5394e
Nedya Prakasa <nedya.prakasa@tokopedia.com>
2021-07-09 20:34:51 +0700

    build: change build and registry target

--- bin/docker-push.sh
+++ bin/docker-push.sh
@@ -7,11 +7,11 @@ source supported_versions
 function docker_push() {
     TAG=$1
     EXITCODE=$(
-        docker push fabiocicerchia/nginx-lua:"$TAG" >/dev/null
+        docker push prakasa1904/nginx-lua:"$TAG" >/dev/null
         echo $?
     )
     if [ "$EXITCODE" -ne "0" ]; then
-        docker push fabiocicerchia/nginx-lua:"$TAG"
+        docker push prakasa1904/nginx-lua:"$TAG"
     fi
 }
 
@@ -21,31 +21,31 @@ function push() {
     PATCH="$NGINX_VER"
 
     if [ "$FORCE" == "0" ]; then
-        if [ "$(docker_tag_exists fabiocicerchia/nginx-lua "$PATCH-$OS$OS_VER")" == "0" ]; then
+        if [ "$(docker_tag_exists prakasa1904/nginx-lua "$PATCH-$OS$OS_VER")" == "0" ]; then
             return
         fi
     fi
 
-    [[ $(docker image ls -q fabiocicerchia/nginx-lua:"$MAJOR-$OS$OS_VER" | wc -l) -ne 0 ]] && docker_push "$MAJOR-$OS$OS_VER"
-    [[ $(docker image ls -q fabiocicerchia/nginx-lua:"$MINOR-$OS$OS_VER" | wc -l) -ne 0 ]] && docker_push "$MINOR-$OS$OS_VER"
-    [[ $(docker image ls -q fabiocicerchia/nginx-lua:"$PATCH-$OS$OS_VER" | wc -l) -ne 0 ]] && docker_push "$PATCH-$OS$OS_VER"
+    [[ $(docker image ls -q prakasa1904/nginx-lua:"$MAJOR-$OS$OS_VER" | wc -l) -ne 0 ]] && docker_push "$MAJOR-$OS$OS_VER"
+    [[ $(docker image ls -q prakasa1904/nginx-lua:"$MINOR-$OS$OS_VER" | wc -l) -ne 0 ]] && docker_push "$MINOR-$OS$OS_VER"
+    [[ $(docker image ls -q prakasa1904/nginx-lua:"$PATCH-$OS$OS_VER" | wc -l) -ne 0 ]] && docker_push "$PATCH-$OS$OS_VER"
 
     if [ "$LAST_VER_NGINX$LAST_VER_OS$DEFAULT_IMAGE" == "111" ]; then
-        [[ $(docker image ls -q fabiocicerchia/nginx-lua:"$MAJOR" | wc -l) -ne 0 ]] && docker_push "$MAJOR"
-        [[ $(docker image ls -q fabiocicerchia/nginx-lua:"$MINOR" | wc -l) -ne 0 ]] && docker_push "$MINOR"
-        [[ $(docker image ls -q fabiocicerchia/nginx-lua:"$PATCH" | wc -l) -ne 0 ]] && docker_push "$PATCH"
-        [[ $(docker image ls -q fabiocicerchia/nginx-lua:latest | wc -l) -ne 0 ]] && docker push fabiocicerchia/nginx-lua:latest
+        [[ $(docker image ls -q prakasa1904/nginx-lua:"$MAJOR" | wc -l) -ne 0 ]] && docker_push "$MAJOR"
+        [[ $(docker image ls -q prakasa1904/nginx-lua:"$MINOR" | wc -l) -ne 0 ]] && docker_push "$MINOR"
+        [[ $(docker image ls -q prakasa1904/nginx-lua:"$PATCH" | wc -l) -ne 0 ]] && docker_push "$PATCH"
+        [[ $(docker image ls -q prakasa1904/nginx-lua:latest | wc -l) -ne 0 ]] && docker push prakasa1904/nginx-lua:latest
     fi
 
     if [ "$LAST_VER_NGINX$LAST_VER_OS" == "11" ]; then
-        [[ $(docker image ls -q fabiocicerchia/nginx-lua:"$OS" | wc -l) -ne 0 ]] && docker_push "$OS"
-        [[ $(docker image ls -q fabiocicerchia/nginx-lua:"$MAJOR-$OS" | wc -l) -ne 0 ]] && docker_push "$MAJOR-$OS"
-        [[ $(docker image ls -q fabiocicerchia/nginx-lua:"$MAJOR-$OS$OS_VER" | wc -l) -ne 0 ]] && docker_push "$MAJOR-$OS$OS_VER"
+        [[ $(docker image ls -q prakasa1904/nginx-lua:"$OS" | wc -l) -ne 0 ]] && docker_push "$OS"
+        [[ $(docker image ls -q prakasa1904/nginx-lua:"$MAJOR-$OS" | wc -l) -ne 0 ]] && docker_push "$MAJOR-$OS"
+        [[ $(docker image ls -q prakasa1904/nginx-lua:"$MAJOR-$OS$OS_VER" | wc -l) -ne 0 ]] && docker_push "$MAJOR-$OS$OS_VER"
     fi
 
     if [ "$LAST_VER_OS" == "1" ]; then
-        [[ $(docker image ls -q fabiocicerchia/nginx-lua:"$MINOR-$OS" | wc -l) -ne 0 ]] && docker_push "$MINOR-$OS"
-        [[ $(docker image ls -q fabiocicerchia/nginx-lua:"$PATCH-$OS" | wc -l) -ne 0 ]] && docker_push "$PATCH-$OS"
+        [[ $(docker image ls -q prakasa1904/nginx-lua:"$MINOR-$OS" | wc -l) -ne 0 ]] && docker_push "$MINOR-$OS"
+        [[ $(docker image ls -q prakasa1904/nginx-lua:"$PATCH-$OS" | wc -l) -ne 0 ]] && docker_push "$PATCH-$OS"
     fi
 }
 
