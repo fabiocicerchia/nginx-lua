c826df3fe561e06dc97ab8e2e2a3c93503a84387
Fabio Cicerchia <info@fabiocicerchia.it>
2023-10-31 21:09:39 +0100

    fixing the script to run on mulitple os

--- src/packages/after-install.sh
+++ src/packages/after-install.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
-source /etc/os-release
+. /etc/os-release
 
-if [ "$ID" == "alpine" ]; then
+if [ "$ID" = "alpine" ]; then
     addgroup -g 101 -S nginx
     adduser -S -D -H -u 101 -h /var/cache/nginx -s /sbin/nologin -G nginx -g nginx nginx
-elif [ "$ID_LIKE" == "debian" ]; then
+elif [ "$ID_LIKE" = "debian" ]; then
     addgroup --system --gid 101 nginx
     adduser --system --disabled-login --ingroup nginx --no-create-home --home /nonexistent --gecos "nginx user" --shell /bin/false --uid 101 nginx
 elif [ -x /usr/sbin/useradd ]; then
