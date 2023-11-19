74b1b373153143ba07fdd0de75b7dadc1ab70e03
Fabio Cicerchia <info@fabiocicerchia.it>
2023-11-19 11:49:15 +0100

    loading correct images in bundle job

--- bin/common.py
+++ bin/common.py
@@ -16,6 +16,7 @@ image_repo = "fabiocicerchia/nginx-lua"
 
 
 def run_command(command, print_stdout):
+    print("running command: %s" % (command))
     process = subprocess.Popen(
         shlex.split(command), shell=False, stdout=subprocess.PIPE
     )
@@ -188,7 +189,7 @@ def push(nginx_ver, os_distro, os_ver):
 
 def docker_bundle(tag):
     tag_amd64 = "%s-amd64" % (tag)
-    tag_arm64 = "%s-arm64" % (tag)
+    tag_arm64 = "%s-arm64v8" % (tag)
 
     exit_code = run_command(
         """
