4c2f8a565c3bbe58a05c11228c69c641ee1b23ae
Fabio Cicerchia <info@fabiocicerchia.it>
2023-12-24 17:52:09 +0100

    added lua-resty-upload

--- bin/test.sh
+++ bin/test.sh
@@ -144,6 +144,7 @@ function exec_tests() {
     curl -v --fail http://localhost:8080/limit-1 || handle_error
     curl -v --fail http://localhost:8080/limit-2 || handle_error
     curl -v --fail http://localhost:8080/limit-3 || handle_error
+    curl -v --fail http://localhost:8080/upload || handle_error
 }
 
 function test_docker_image() {
