d7e6d9a319bfba36b2f3defda9db48c896fec644
Fabio Cicerchia <info@fabiocicerchia.it>
2024-01-09 22:51:24 +0100

    fixed typo

--- bin/test.sh
+++ bin/test.sh
@@ -144,7 +144,7 @@ function exec_tests() {
     curl -v --fail http://localhost:8080/limit-1 || handle_error
     curl -v --fail http://localhost:8080/limit-2 || handle_error
     curl -v --fail http://localhost:8080/limit-3 || handle_error
-    curl -v --fail http://localhost:8080/lrucahe || handle_error
+    curl -v --fail http://localhost:8080/lrucache || handle_error
     curl -v --fail http://localhost:8080/signal || handle_error
     curl -v --fail http://localhost:8080/tablepool || handle_error
 }
